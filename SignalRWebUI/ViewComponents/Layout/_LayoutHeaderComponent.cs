9d12a2a7e12ad1f4ec74be3cfbfa0c42c7a28cd2
sedatbilece <seda1tbilece@gmail.com>
2024-02-06 20:11:51 +0300

    add Layout at WebUI

---
+++ SignalRWebUI/ViewComponents/Layout/_LayoutHeaderComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.Layout
+{
+    public class _LayoutHeaderComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRWebUI/ViewComponents/Layout/_LayoutSideBarComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.ViewComponents.Layout
+{
+	public class _LayoutSideBarComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
