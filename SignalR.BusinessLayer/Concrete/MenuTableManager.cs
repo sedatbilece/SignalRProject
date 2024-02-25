40f39ac05e996aaae0eff7e4ebc4c116f804438e
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-02-25 20:31:00 +0300

    Add MenuTable entity and ef structure

---
+++ SignalR.BusinessLayer/Concrete/MenuTableManager.cs
@@ -0,0 +1,53 @@
+﻿using SignalR.BusinessLayer.Abstract;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.EntityFramework;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Concrete
+{
+	public class MenuTableManager : IMenuTableService
+	{
+		private readonly IMenuTableDal _efMenuTableDal;
+
+		public MenuTableManager(IMenuTableDal efMenuTableDal)
+		{
+			_efMenuTableDal = efMenuTableDal;
+		}
+
+		public int MenuTableCount()
+		{
+			return _efMenuTableDal.MenuTableCount();
+		}
+
+		public void TAdd(MenuTable entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void TDelete(MenuTable entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<MenuTable> TGetAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public MenuTable TGetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void TUpdate(MenuTable entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+}
---
+++ SignalR.DataAccessLayer/EntityFramework/EfMenuTableDal.cs
@@ -0,0 +1,22 @@
+﻿using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+	public class EfMenuTableDal : GenericRepository<MenuTable>, IMenuTableDal
+	{
+		private readonly SignalRContext _context;
+		public EfMenuTableDal(SignalRContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		public int MenuTableCount()
+		{
+			return _context.MenuTables.Count();
+		}
+	}
+
+}
