1b4bb6c32daecaeac6a3909da8c4247897b88245
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-02-28 20:21:35 +0300

    add UI pages (DefaultController)

---
+++ SignalRWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUI.Controllers
+{
+	public class DefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+        public IActionResult Menu()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult BookTable()
+        {
+            return View();
+        }
+    }
+}
