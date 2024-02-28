1d93adcedfb310f767c27ff3cde2cd3849ff094a
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-02-28 21:13:36 +0300

    add Slider entity, logic and UI

--- SignalRWebUI/Controllers/DefaultController.cs
+++ SignalRWebUI/Controllers/DefaultController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.DefaultDtos;
+using SignalRWebUI.Dtos.DefaultDtos.SliderDtos;
 
 namespace SignalRWebUI.Controllers
 {
 	public class DefaultController : Controller
 	{
-		public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
 		{
-			return View();
+            var model = new DefaultDto();
+
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://localhost:7298/api/Sliders");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSliderDto>>(jsonData);
+                model.Sliders = values;
+            }
+
+
+
+            return View(model);
 		}
         public IActionResult Menu()
         {
---
+++ SignalRWebUI/Dtos/DefaultDtos/DefaultDto.cs
@@ -0,0 +1,9 @@
+﻿using SignalRWebUI.Dtos.DefaultDtos.SliderDtos;
+
+namespace SignalRWebUI.Dtos.DefaultDtos
+{
+    public class DefaultDto
+    {
+        public List<ResultSliderDto> Sliders { get; set; }
+    }
+}
