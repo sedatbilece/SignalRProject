c767d263435c90ae5a2ffb48e3bf026fc63b263d
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-03-06 17:20:28 +0300

    add notification entity and db logic

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -72,6 +72,9 @@ builder.Services.AddScoped<ISliderDal, EfSliderDal>();
 builder.Services.AddScoped<IBasketService, BasketManager>();
 builder.Services.AddScoped<IBasketDal, EfBasketDal>();
 
+builder.Services.AddScoped<INotificationService, NotificationManager>();
+builder.Services.AddScoped<INotificationDal, EfNotificationDal>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
