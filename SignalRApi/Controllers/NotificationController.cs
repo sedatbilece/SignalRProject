8ea987d2ee34ebff74dd26502dca08cb3fc0a58c
Sedat Bilece <63735861+sedatbilece@users.noreply.github.com>
2024-03-09 12:01:41 +0300

    add(Notification) : ui and api endpoints

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
 using SignalR.EntityLayer.Entities;
 
 namespace SignalRApi.Controllers
@@ -30,6 +31,49 @@ namespace SignalRApi.Controllers
         public IActionResult GetAllByStatus()
         {
             return Ok(_notificationService.GetAllNotificationByStatus(false));
+        }
+
+        [HttpPost]
+        public IActionResult Add(CreateNotificationDto notification)
+        {
+            _notificationService.TAdd(new Notification
+            {
+                Date = notification.Date,
+                Description = notification.Description,
+                Icon = notification.Icon,
+                Status = notification.Status,
+                Type = notification.Type
+            });
+            return Created("Created notification", notification);
+        }
+
+        [HttpPut]   
+        public IActionResult Update(UpdateNotificationDto notification)
+        {
+            var notificationToUpdate = _notificationService.TGetById(notification.Id);
+            if (notificationToUpdate == null)
+            {
+                return NotFound();
+            }
+            notificationToUpdate.Date = notification.Date;
+            notificationToUpdate.Description = notification.Description;
+            notificationToUpdate.Icon = notification.Icon;
+            notificationToUpdate.Status = notification.Status;
+            notificationToUpdate.Type = notification.Type;
+            _notificationService.TUpdate(notificationToUpdate);
+
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var notificationToDelete = _notificationService.TGetById(id);
+            if (notificationToDelete == null)
+            {
+                return NotFound();
+            }
+            _notificationService.TDelete(notificationToDelete);
+            return NoContent();
         }
 
 
---
+++ SignalRWebUI/Controllers/NotificationController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.DtoLayer.NotificationDtos;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+    public class NotificationController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public NotificationController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync("https://localhost:7298/api/notification");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultNotificationDto>>(jsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateNotification()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNotification(CreateNotificationDto model)
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("https://localhost:7298/api/Notification", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DeleteNotification(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.DeleteAsync($"https://localhost:7298/api/Notification/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> UpdateNotification(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var response = await client.GetAsync($"https://localhost:7298/api/Notification/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonData = await response.Content.ReadAsStringAsync();
+                var value = JsonConvert.DeserializeObject<UpdateNotificationDto>(jsonData);
+                return View(value);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateNotification(UpdateNotificationDto model)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(model);
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await client.PutAsync("https://localhost:7298/api/Notification", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
